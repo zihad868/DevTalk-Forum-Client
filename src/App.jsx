46143db40cc821e84adf4e2413e4cb36a088cccb
zihad868 <zihadh868@gmail.com>
2024-06-02 17:58:11 +0600

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Button</button>
     </>
   );
 }
