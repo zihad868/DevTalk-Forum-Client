67b21ec527c18fa86dba78f19a56d8a60c25d440
zihad868 <zihadh868@gmail.com>
2024-06-04 21:49:47 +0600

    Make Admin User

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,8 +1,11 @@
+import Swal from "sweetalert2";
 import useAllUsers from "../../hooks/useAllUsers";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 import Loading from "../../shared/Loading/Loading";
 import { RiAdminFill } from "react-icons/ri";
 
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
   const [users, refetch, isPending] = useAllUsers();
 
   if (isPending) {
@@ -10,7 +13,29 @@ const AllUsers = () => {
   }
 
   const handleMakeAdmin = (id) => {
-    console.log(id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Make Admin!"
+    }).then(async(result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.patch(`/users/admin/${id}`)
+        if(res.data.modifiedCount > 0){
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Make Admin Success",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      }
+    });
   }
 
   return (
@@ -51,7 +76,7 @@ const AllUsers = () => {
                  {
                     user?.role === 'admin' ? 'Admin' : 
                     <>
-                      <button onClick={handleMakeAdmin} className="btn btn-secondary"><RiAdminFill />Make Admin</button>
+                      <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-secondary"><RiAdminFill />Make Admin</button>
                     </>
                  }
               </th>
