c819b2e9ded88d82ccf260610e2dd26decf8dbf6
zihad868 <zihadh868@gmail.com>
2024-06-06 12:04:23 +0600

    Select Option React Select

--- src/pages/Dashboard/AddPost.jsx
+++ src/pages/Dashboard/AddPost.jsx
@@ -4,9 +4,13 @@ import SectionTitle from "../../shared/SectionTitle/SectionTitle";
 import Select from 'react-select';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: 'javascript', label: 'JavaScript' },
+  { value: 'react', label: 'React' },
+  { value: 'python', label: 'Python' },
+  { value: 'django', label: 'Django' },
+  { value: 'flask', label: 'Flask' },
+  { value: 'c', label: 'C' },
+  { value: 'ruby', label: 'Ruby' },
 ];
 
 const AddPost = () => {
